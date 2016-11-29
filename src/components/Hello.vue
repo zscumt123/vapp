c7c372864c4aa08c6f4db8bbf63d050e0687eec5
zs <zscumt123@163.com>
2016-11-29 21:57:06 +0800

    change hello.vue

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -8,14 +8,14 @@
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter,hello</a></li>
       <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docsfor This Template</a></li>
     </ul>
     <h2>Ecosystem</h2>
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">helloawesome-vue</a></li>
     </ul>
   </div>
 </template>
