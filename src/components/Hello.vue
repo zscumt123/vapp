b6566702bef4cd20f5e6cdad40a0d92280476351
zs <zscumt123@163.com>
2016-11-30 17:23:08 +0800

    hello.vue

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -16,7 +16,7 @@
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue--router,,</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex.</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader,helo</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader,helo,ok</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank"> helloawesome,hello</a></li>
     </ul>
   </div>
